e355730d385889bde371fc8637ada79204b8eb47
YavorivIhor <87531725+YavorivIhor@users.noreply.github.com>
2021-07-16 17:35:31 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
 import React from 'react';
+import './App.css'
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDWBzvKg1f8-8Ijsyi5JoU6oYDziSZ3EyQ",
+  authDomain: "test-app-4b520.firebaseapp.com",
+  databaseURL: "https://test-app-4b520-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "test-app-4b520",
+  storageBucket: "test-app-4b520.appspot.com",
+  messagingSenderId: "116274295112",
+  appId: "1:116274295112:web:c21a041c70eff0d216c37d",
+  measurementId: "G-4NWJQVGJPY"
+})
+
+const firestore = firebase.firestore();
+
 class Form extends React.Component {
   constructor() {
     super();
@@ -30,9 +48,9 @@ class Form extends React.Component {
 
   render() {
     return (
-      <div id="mainContainer">
+      <div id="mainContainer" >
           <textarea rows="2" cols="20" value={this.state.Value} onChange={this.handleChange} ></textarea>
-          <div >
+          <div>
           <input  type="submit" className="button" onClick={this.appendData}  value="Додати"/>
           </div>
           <div id="data-Container">
